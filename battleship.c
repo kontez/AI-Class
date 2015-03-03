013b7d134f46806915cfa673906beaa402ed4529
Marvin Contessi <kontez@users.noreply.github.com>
2015-03-03 11:43:42 +0000

    New Algorithm
    Made some major improvements, changed the algorithm with a better one, needs still some fixing though

--- battleship.c
+++ battleship.c
@@ -7,10 +7,24 @@
  * Marvin Contessi
  * Contact:
  * kontez@autistici.org
+ *   
+ * USAGE:
+ * The program wants to be fed with a file 
+ * that contains 14 coordinates with no spaces
+ * (e.g. B5B6B7B8B9C3D3E3F3F7G7H7J7J8 
+ * 
+ * then you can compile it with:
+ * gcc battleship.c -o battleship
+ *
+ * and execute it with:
+ * ./battleship filename
+ *
+ * have fun :)
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main( int argc, char *argv[] )
 {
@@ -19,9 +33,17 @@ int main( int argc, char *argv[] )
     int board[10][10];        /* this will be the board */
     int x = 0;                /* coordinates */
     int y = 0;
-    int i = 0;
+    int i = 0;                /* counters */
     int j = 0;
-    int shots = 0;
+    int k = 0;
+    int c = 0;       
+    int ships = 0;            /* total fields to hit */
+    int shots = 0;            /* shots fired */
+    int dir = 0;              /* direction
+                               * 0 - horizontal
+                               * 1 - vertical
+                               */
+    srand((unsigned)time(0));
 
     /* Initializing the board */
     for (i = 0; i < 10; i++)
@@ -30,8 +52,7 @@ int main( int argc, char *argv[] )
         {
             board[i][j] = 0;
         }
-    } 
-    printf("board cleared\n");
+    }
 
     /* loading the file */
     coordinates = fopen(filename,"r");
@@ -48,23 +69,23 @@ int main( int argc, char *argv[] )
     y = (int) getc(coordinates) - 48;
     while(!feof(coordinates))
     {
-        printf("x = %d; y = %d\n",x,y);
         board[x][y] = 1;
         x = (int) getc(coordinates) - 65;
         y = (int) getc(coordinates) - 48;
         
     } 
-    printf("board loaded!\n");
+    printf("\n[board loaded!]\n");
+    printf("board layout: \n");
 
 
     /* Print the board */
     for (i = 0; i < 10; i++)
     {
-        printf("\n");
         for (j = 0; j < 10; j++)
         {
             printf("%d ",board[i][j]);
         }
+        printf("\n");
     }
 
     printf("\n");
@@ -75,35 +96,149 @@ int main( int argc, char *argv[] )
     {
         for (j = 0; j < 10; j++)
         {
-            if ( (i + j)%2 == 0 ) /* if the box is even */
+            if ( ((i + j)%2 == 0) && (ships < 14 )) /* if the box is even 
+                                                     * and the number of ships that I hit
+                                                     * is below 14
+                                                     * where 14 is the total number of 
+                                                     * fields that contain ships
+                                                    */
             {
                 if ( board[i][j] == 1) /* ship */
                 {
                     board[i][j] = -2; /* hit */
+                    ships++;
                     shots++;
 
-                    if ( (board[i][j+1] == 1) && (j < 10) ) /* right */
-                    {
-                        board[i][j+1] = -2;
-                        shots++;
-                    }                             
-                    
-                    if ( (board[i][j-1] == 1) && (j > 0) ) /* left */
-                    {
-                        board[i][j-1] = -2;
-                        shots++;
-                    }
+                    dir = rand()%2; /* select direction */
 
-                    if ( (board[i+1][j] == 1) && (i < 10) ) /* down */
-                    {
-                        board[i+1][j] = -2;
-                        shots++;
-                    }
+                    /* save pos */
+                    x = i;
+                    y = j;
 
-                    if ( (board[i-1][j] == 1) && (i > 0) ) /* up */
+                    for(c = 0; c < 2; c++) /* do it 2 times */
                     {
-                        board[i-1][j] = -2;
-                        shots++;
+                        if(dir == 1) /* vertical */
+                        {
+                            if ( (board[x-1][y] == 1) && (x > 0)) /* up hit */
+                            {
+                                shots++;
+                                board[x-1][y] = -2; /* hit */
+                                ships++;
+                                k = x - 2;
+                                do /* attack up until there's water */
+                                {
+                                    if ((board[k][y] == 1) && (k > 0)) /* if ship */
+                                    {
+                                        board[k][y] = -2;
+                                        shots++;
+                                        ships++;
+                                        k--;
+                                    }
+                                    else if ((board[k][y] == 0) && (k > 0)) /* if water */
+                                    {
+                                        board[k][y] = -1;
+                                        shots++;
+                                    }
+                                } while ((board[k][y] == 1) && (k > 0));
+                            }
+                            else if ( (board[x-1][y] == 0) && (x > 0)) /* up miss */
+                            {
+                                /* miss and attack down */
+                                board[x-1][y] = -1;
+                                shots++;
+                                if ( (board[x+1][y] == 1) && (x < 10)) /* if ship */
+                                {
+                                    /* attack down */
+                                    board[x+1][y] = -2;
+                                    ships++;
+                                    shots++;
+                                    k = x + 2;   
+                                    do /* attack down until there's water */
+                                    {
+                                        if ((board[k][y] == 1) && (k < 10)) /* if ship */
+                                        {
+                                            board[k][y] = -2;
+                                            shots++;
+                                            ships++;
+                                            k++;
+                                        }
+                                        else if ((board[k][y] == 0) && (k < 10)) /* if water */
+                                        {
+                                            board[k][y] = -1;
+                                            shots++;
+                                        }
+                                    } while ((board[k][y] == 1) && (k < 10));                                 
+                                }
+                                else if ( (board[x+1][y] == 0) && (x < 10))/* if miss, change direction */
+                                {
+                                    /* miss */
+                                    board[x+1][y] = -1;
+                                    shots++;
+                                    dir = 0;
+                                }
+                            }
+                        }
+
+                        else if (dir == 0) /* horizontal */
+                        {
+                            if ( (board[x][y-1] == 1) && (y > 0)) /* left attack */
+                            {
+                                board[x][y-1] = -2;
+                                shots++;
+                                ships++;
+                                k = y - 2;
+                                do /* attack left until there's water */
+                                {
+                                    if ((board[x][k] == 1) && (k > 0)) /* if ship */
+                                    {
+                                        board[x][k] = -2;
+                                        shots++;
+                                        ships++;
+                                        k--;
+                                    }
+                                    else if ((board[x][k] == 0) && (k > 0)) /* if water */
+                                    {
+                                        board[x][k] = -1;
+                                        shots++;
+                                    }
+                                } while ((board[x][k] == 1) && (k > 0));
+                            }
+                            else if ( (board[x][y-1] == 0) && (y > 0)) /* if left miss */
+                            {
+                                /* miss and attack right */
+                                board[x][y-1] = -1;
+                                shots++;
+                                if ( (board[x][y+1] == 1) && (y < 10)) /* attack right */
+                                {
+                                    board[x][y+1] = -2;
+                                    shots++;
+                                    ships++;
+                                    k = y + 2;   
+                                    do /* attack right until there's water */
+                                    {
+                                        if ((board[x][k] == 1) && (k < 10)) /* if ship */
+                                        {
+                                            board[x][k] = -2;
+                                            shots++;
+                                            ships++;
+                                            k++;
+                                        }
+                                        else if ((board[x][k] == 0) && (k < 10)) /* if water */
+                                        {
+                                            board[x][k] = -1;
+                                            shots++;
+                                        }
+                                    } while ((board[x][k] == 1) && (k < 10));                                 
+                                }
+                                else if ( (board[x][y+1] == 0) && (y < 10))
+                                {
+                                    /* miss */
+                                    board[x][y+1] = -1;
+                                    shots++;
+                                    dir = 1;
+                                }
+                            }
+                        }
                     }
                 }
 
@@ -122,6 +257,8 @@ int main( int argc, char *argv[] )
 
 
     /* reprint the board for debugging */
+    printf("board after attack: \n");
+    printf("[ 0 -> water | -1 -> miss | -2 -> hit ]\n");
     for (i = 0; i < 10; i++)
     {
         printf("\n");
